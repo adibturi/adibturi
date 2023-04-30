a5ba3928482e8a419ba2fe0c53bc2f0357347bab
adibturi <adibturi16@gmail.com>
2023-04-30 18:59:30 +0700

    tugas akhir ppweb

---
+++ resources/views/portofolio.blade.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>PORTOFOLIO | MUHAMMAD ADIB TURI</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
+  <link href="assets/img/foto2.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: MyResume
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <!-- <button type="button" class="mobile-nav-toggle d-xl-none"><i class="bi bi-list mobile-nav-toggle"></i></button> -->
+  <i class="bi bi-list mobile-nav-toggle d-lg-none"></i>
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex flex-column justify-content-center">
+
+    <nav id="navbar" class="navbar nav-menu">
+      <ul>
+        <li><a href="/" class="nav-link scrollto {{ ($title === "home") ? 'active' : '' }}"><i class="bx bx-home"></i> <span>Home</span></a></li>
+        <li><a href="profile" class="nav-link scrollto {{ ($title === "profile") ? 'active' : '' }}"><i class="bx bx-user"></i> <span>About</span></a></li>
+        <li><a href="portofolio" class="nav-link scrollto {{ ($title === "portofolio") ? 'active' : '' }}"><i class="bx bx-book-content"></i> <span>Portofolio</span></a></li>
+        <li><a href="contact" class="nav-link scrollto {{ ($title === "contact") ? 'active' : '' }}"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+      </ul>
+    </nav><!-- .nav-menu -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center">
+    <div class="container" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Muhammad Adib Turi</h1>
+      <p>I'm <span class="typed" data-typed-items="Mahasiswa,"></span></p>
+      <div class="social-links">
+        <a href="http://twitter.com" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="http://facebook.com" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="http://instagram.com" class="instagram"><i class="bx bxl-instagram"></i></a>
+        
+      </div>
+      <div class="mt-3"><a href="contact">Contact Me</a></div>
+    </div>
+  </section><!-- End Hero -->
+
+  </main><!-- End #main -->
+
+      <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row">
+            <h3 class="resume-title">Sumary</h3>
+            <div class="resume-item pb-0">
+              <h4>Muhammad Adib Turi</h4>
+              <p><em>Saya lahir dipalembang pada 10 November 2003, saya anak pertama dari 2 bersaudara ,hobi saya sepak bola dan badminton</em></p>
+              <ul>
+                <li>+62 809 1234 6789</li>
+                <li>adibturi16@gmail.com</li>
+              </ul>
+            </div>
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>Mahasiswa</h4>
+              <h5>2021 - Sekarang</h5>
+              <p><em>Universitas Sriwijaya Palembang</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Siswa</h4>
+              <h5>2018 - 2021</h5>
+              <p><em>SMA Patra Mandiri 01 Palembang</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Siswa</h4>
+              <h5>2015 - 2018</h5>
+              <p><em>SMP Negeri 18 Palembang</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Siswa</h4>
+              <h5>2009 - 2015</h5>
+              <p><em>SD Negeri 06 Palembang</em></p>
+            </div>
+          </div>
+    </section><!-- End Resume Section -->
+  </main><!-- End #main -->
+
+  <!-- ======= Skills Section ======= -->
+    <section id="skills" class="skills section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Skills</h2>
+          <p>Menurut apa yang sudah saya pelajari dalam beberapa semester ini, dapat simpulkan bahwa pemahaman saya terhadap beberapa bahasa pemograman memiliki presentase sebagai berikut!</p>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6">
+
+            <div class="progress">
+              <span class="skill">HTML <i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            
+
+          </div>
+
+          <div class="col-lg-6">
+
+            <div class="progress">
+              <span class="skill">PHP <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+            <div class="progress">
+              <span class="skill">JavaScript <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Skills Section -->
+
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Muhammad Adib Turi</h3>
+      <p>"Belajar dari kegagalan adalah hal yang bijak."</p>
+      <div class="social-links">
+        <a href="http://twitter.com" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="http://facebook.com" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="http://instagram.com" class="instagram"><i class="bx bxl-instagram"></i></a>
+      <div class="copyright mt-3">
+        &copy; Copyright <strong><span>MyResume</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: [license-url] -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/ -->
+        Designed by Muhammad Adib Turi
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+          <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
